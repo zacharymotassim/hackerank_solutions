73ffcfa478fff620e533e5f5f4ce48fe104f2302
zacharymotassim <65255629+zacharymotassim@users.noreply.github.com>
2022-08-16 19:22:41 -0400

    Add files via upload

---
+++ diagonal_difference.py
@@ -0,0 +1,11 @@
+def diagonalDifference(arr):
+    left_to_right = 0
+    right_to_left = 0
+    for i in range(len(arr)):
+        for j in range(len(arr)):
+            if(i==j):# coordinates for left to right
+                left_to_right+=arr[i][j]
+            if(j==len(arr)-1-i): # coordinates for right to left
+                  right_to_left+=arr[i][j]
+    result=left_to_right-right_to_left
+    return result if result>0 else result*-1
