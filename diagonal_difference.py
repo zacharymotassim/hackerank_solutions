2ac222d3f3e70196e390d7735e99ad2639d9cc8a
zacharymotassim <65255629+zacharymotassim@users.noreply.github.com>
2022-08-16 19:23:16 -0400

    Update diagonal_difference.py

--- diagonal_difference.py
+++ diagonal_difference.py
@@ -2,7 +2,7 @@ def diagonalDifference(arr):
     left_to_right = 0
     right_to_left = 0
     for i in range(len(arr)):
-        for j in range(len(arr)):
+        for j in range(len(arr[i])):
             if(i==j):# coordinates for left to right
                 left_to_right+=arr[i][j]
             if(j==len(arr)-1-i): # coordinates for right to left
